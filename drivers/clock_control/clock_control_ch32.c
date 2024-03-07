b13c8192f0646c0d2762343ed45ae7f7b5dc4aab
rt4bc <bochao.me@gmail.com>
2024-03-07 20:45:19 +0800

    update clock control init functions

--- drivers/clock_control/clock_control_ch32.c
+++ drivers/clock_control/clock_control_ch32.c
@@ -15,154 +15,95 @@
 
 #include <zephyr/dt-bindings/clock/ch32_clock.h>
 
+#define CPU_FREQ DT_PROP(DT_PATH(cpus, cpu_0), clock_frequency)
+#define HSE_FREQ DT_PROP(DT_PATH(clocks, clk_hse), clock_frequency)
+#define HSI_FREQ DT_PROP(DT_PATH(clocks, clk_hsi), clock_frequency)
+#define LSI_FREQ DT_PROP(DT_PATH(clocks, clk_lsi), clock_frequency)
+#define LSE_FREQ DT_PROP(DT_PATH(clocks, clk_lse), clock_frequency)
+#define PLL1_MULT          DT_PROP(DT_PATH(clocks, pll1), clock_mult)
+
+#define RCC_AHB_PRESCALER  DT_PROP(DT_PATH(soc, reset_clock_controller_40021000), ahb_prescaler)
+#define RCC_APB1_PRESCALER DT_PROP(DT_NODELABEL(rcc), apb1_prescaler)
+#define RCC_APB2_PRESCALER DT_PROP(DT_NODELABEL(rcc), apb2_prescaler)
+
+#define RCC_CLOCKS_OUTPUT  DT_PROP(DT_PATH(soc, rcc), clock_frequency)
+#define RCC_CLOCKS_SRC     DT_PHANDLE(DT_NODELABEL(rcc), clocks)
+#define CLK_HSE_NODE       DT_NODELABEL(clk_hse)
+#define CLK_HSI_NODE       DT_NODELABEL(clk_hsi)
+#define PLL1_NODE          DT_NODELABEL(pll1)
+
+
+#ifdef CONFIG_RISCV
+#include "ch32v30x_rcc.h"
+
+#ifdef CH32V30x_D8C
+#define RCC_PLL1_MUL _CONCAT(_CONCAT(RCC_PLLMul_, PLL1_MULT), _EXTEN)
+#else
+#define RCC_PLL1_MUL _CONCAT(RCC_PLLMul_, PLL1_MULT)
+#endif
+
+#define RCC_SYSCLK_DIV _CONCAT(RCC_SYSCLK_Div, RCC_AHB_PRESCALER)
+#define RCC_HCLK_DIV _CONCAT(RCC_HCLK_Div, RCC_APB1_PRESCALER)
+#define RCC_PCLK2_DIV _CONCAT(RCC_HCLK_Div, RCC_APB2_PRESCALER)
+
+#endif
+
 static int clock_control_ch32_init(const struct device *dev)
 {
-    #if 0
-#ifdef CONFIG_DT_HAS_WCH_CH32V3X_PLL_CLOCK_ENABLED
-	uint32_t prediv, pllmul;
-	uint32_t ahb_div, apb1_div, apb2_div;
+	ErrorStatus ret;
 
-	RCC_HSEConfig(RCC_HSE_ON);
-	do{
-	}while(RCC_WaitForHSEStartUp()!=READY);
+	if (DT_NODE_HAS_STATUS(DT_PATH(clocks, pll1), okay)) {
+		/*RCC_PLLSource_PREDIV1 defaut is HSE*/
+		RCC_HSEConfig(RCC_HSE_ON);
+		do {
+			ret = RCC_WaitForHSEStartUp();
+		} while (ret != READY);
 
-	BUILD_ASSERT(CPU_FREQ == ((HSE_FREQ) * (PLL1_MUL1) / (PLL1_PREDIV1)) , 
-		"Unexpected CPU_FREQ and PLL Configuration, Please Check DTS");
-	
-	switch (PLL1_MUL1)
-	{
-		case 3: pllmul = RCC_PLLMul_3_EXTEN; break;
-		case 4: pllmul = RCC_PLLMul_4_EXTEN; break;
-		case 5: pllmul = RCC_PLLMul_5_EXTEN; break;
-		case 6: pllmul = RCC_PLLMul_6_EXTEN; break;
-		case 7: pllmul = RCC_PLLMul_7_EXTEN; break;
-		case 8: pllmul = RCC_PLLMul_8_EXTEN; break;
-		case 9: pllmul = RCC_PLLMul_9_EXTEN; break;
-		case 10: pllmul = RCC_PLLMul_10_EXTEN; break;
-		case 11: pllmul = RCC_PLLMul_11_EXTEN; break;
-		case 12: pllmul = RCC_PLLMul_12_EXTEN; break;
-		case 13: pllmul = RCC_PLLMul_13_EXTEN; break;
-		case 14: pllmul = RCC_PLLMul_14_EXTEN; break;
-		case 15: pllmul = RCC_PLLMul_15_EXTEN; break;
-		case 16: pllmul = RCC_PLLMul_16_EXTEN; break;
-		// case 17: pllmul = RCC_PLLMul_6_5_EXTEN; break;
-		case 18: pllmul = RCC_PLLMul_18_EXTEN; break;
-		default: pllmul = 0; break;
+		RCC_PLLConfig(RCC_PLLSource_PREDIV1, RCC_PLL1_MUL);
+		RCC_PLLCmd(ENABLE);
 	}
 
-	switch (PLL1_PREDIV1)
-	{
-	case 1: prediv = RCC_PREDIV1_Div1; break;
-	case 2: prediv = RCC_PREDIV1_Div2; break;
-	case 3: prediv = RCC_PREDIV1_Div3; break;
-	case 4: prediv = RCC_PREDIV1_Div4; break;
-	case 5: prediv = RCC_PREDIV1_Div5; break;
-	case 6: prediv = RCC_PREDIV1_Div6; break;
-	case 7: prediv = RCC_PREDIV1_Div7; break;
-	case 8: prediv = RCC_PREDIV1_Div8; break;
-	case 9: prediv = RCC_PREDIV1_Div9; break;
-	case 10: prediv = RCC_PREDIV1_Div10; break;
-	case 11: prediv = RCC_PREDIV1_Div11; break;
-	case 12: prediv = RCC_PREDIV1_Div12; break;
-	case 13: prediv = RCC_PREDIV1_Div13; break;
-	case 14: prediv = RCC_PREDIV1_Div14; break;
-	case 15: prediv = RCC_PREDIV1_Div15; break;
-	case 16: prediv = RCC_PREDIV1_Div16; break;
-	default: prediv = 0;
-		break;
-	}
-	
-	RCC_PLLConfig(prediv, pllmul);
-	RCC_PLLCmd(ENABLE);
+	// configure AHB APB1 and APB2
+	RCC_HCLKConfig(RCC_SYSCLK_DIV);
+	RCC_PCLK1Config(RCC_HCLK_DIV);
+	RCC_PCLK2Config(RCC_PCLK2_DIV);
 
-	switch (AHB_DIV)
-	{
-	case 1: ahb_div = RCC_SYSCLK_Div1; break;
-	case 2: ahb_div = RCC_SYSCLK_Div2; break;
-	case 4: ahb_div = RCC_SYSCLK_Div4; break;
-	case 8: ahb_div = RCC_SYSCLK_Div8; break;
-	case 16: ahb_div = RCC_SYSCLK_Div16; break;
-	case 64: ahb_div = RCC_SYSCLK_Div64; break;
-	case 128: ahb_div = RCC_SYSCLK_Div128; break;
-	case 256: ahb_div = RCC_SYSCLK_Div256; break;
-	case 512: ahb_div = RCC_SYSCLK_Div512; break;
-	default: ahb_div = 0; break;
+	// switch SYSCLK clock
+	if (DT_SAME_NODE(RCC_CLOCKS_SRC, PLL1_NODE)) {
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+	} else if (DT_SAME_NODE(RCC_CLOCKS_SRC, CLK_HSE_NODE)) {
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE);
+	} else {
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
 	}
 
-	switch (APB1_DIV)
-	{
-	case 1: apb1_div = RCC_HCLK_Div1;	break;
-	case 2: apb1_div = RCC_HCLK_Div2;	break;
-	case 4: apb1_div = RCC_HCLK_Div4;	break;
-	case 8: apb1_div = RCC_HCLK_Div8;	break;
-	case 16: apb1_div = RCC_HCLK_Div16;	break;
-	default:
-		break;
-	}
-
-	switch (APB2_DIV)
-	{
-	case 1: apb2_div = RCC_HCLK_Div1;	break;
-	case 2: apb2_div = RCC_HCLK_Div2;	break;
-	case 4: apb2_div = RCC_HCLK_Div4;	break;
-	case 8: apb2_div = RCC_HCLK_Div8;	break;
-	case 16: apb2_div = RCC_HCLK_Div16;	break;
-	default:
-		break;
-	}
-
-	RCC_HCLKConfig(ahb_div);
-	RCC_PCLK1Config(apb1_div);
-	RCC_PCLK2Config(apb2_div);
-
-	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-	/* config AHB & APB1,2 */
-
-#endif
-#endif
 	return 0;
 }
 
-static int clock_control_ch32_on(const struct device *dev,
-				  clock_control_subsys_t sys)
+static int clock_control_ch32_on(const struct device *dev, clock_control_subsys_t sys)
 {
 	ARG_UNUSED(dev);
 	
 	return 0;
 }
 
-static int clock_control_ch32_off(const struct device *dev,
-				   clock_control_subsys_t sys)
+static int clock_control_ch32_off(const struct device *dev, clock_control_subsys_t sys)
 {
 	ARG_UNUSED(dev);
 	return 0;
 }
 
 static enum clock_control_status clock_control_ch32_get_status(const struct device *dev,
-								clock_control_subsys_t sys)
+							       clock_control_subsys_t sys)
 {
 	ARG_UNUSED(dev);
-	// uint32_t clk_en_reg = periph_ll_get_clk_en_reg((periph_module_t)sys);
-	// uint32_t clk_en_mask =  periph_ll_get_clk_en_mask((periph_module_t)sys);
-
-	// if (DPORT_GET_PERI_REG_MASK(clk_en_reg, clk_en_mask)) {
-	// 	return CLOCK_CONTROL_STATUS_ON;
-	// }
 	return CLOCK_CONTROL_STATUS_OFF;
 }
 
-static int clock_control_ch32_get_rate(const struct device *dev,
-					clock_control_subsys_t sub_system,
-					uint32_t *rate)
+static int clock_control_ch32_get_rate(const struct device *dev, clock_control_subsys_t sub_system,
+				       uint32_t *rate)
 {
 	ARG_UNUSED(sub_system);
-
-	// rtc_cpu_freq_config_t config;
-
-	// rtc_clk_cpu_freq_get_config(&config);
-
-	// *rate = config.freq_mhz;
-
 	return 0;
 }
 
@@ -173,9 +114,9 @@ static const struct clock_control_driver_api clock_control_ch32_api = {
 	.get_status = clock_control_ch32_get_status,
 };
 
+// BUILD_ASSERT((VCO_FREQ(pll_sys) >= PLL_VCO_FREQ_MIN) && (VCO_FREQ(pll_sys) <= PLL_VCO_FREQ_MAX)
+// && 		     (VCO_FREQ(pll_sys) >= (CLOCK_FREQ_xosc / REF_DIV(pll_sys) * 16)), "pll_sys:
+// vco_freq is out of range");
 
-DEVICE_DT_INST_DEFINE(0,
-		      &clock_control_ch32_init,
-		      NULL, NULL, NULL,
-		      PRE_KERNEL_1, CONFIG_CLOCK_CONTROL_INIT_PRIORITY,
-		      &clock_control_ch32_api);
+DEVICE_DT_INST_DEFINE(0, &clock_control_ch32_init, NULL, NULL, NULL, PRE_KERNEL_1,
+		      CONFIG_CLOCK_CONTROL_INIT_PRIORITY, &clock_control_ch32_api);
---
+++ soc/riscv/wch_ch32/ch32v3x/pinctrl_soc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * WCH CH32 SoC specific helpers for pinctrl driver
+ */
+
+#ifndef ZEPHYR_SOC_RISCV_WCH_CH32_COMMON_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_RISCV_WCH_CH32_COMMON_PINCTRL_SOC_H_
+
+#include <zephyr/drivers/pinctrl/pinctrl_soc_ch32_common.h>
+
+#endif /* ZEPHYR_SOC_RISCV_WCH_CH32_COMMON_PINCTRL_SOC_H_ */
